5579abbb0c24697cf31bb275cb0d6f81113e0628
GeekTR <geektheripper@gmail.com>
2018-10-20 02:11:28 +0800

    install bbr

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -153,3 +153,36 @@ mu_installs::zsh::install() {
   sudo apt-get install -y zsh && \
   su "$USER" -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
+
+# BBR
+
+mu_installs::xenial_bbr::test() {
+  if [[ $(lsb_release -s -d) != *"Ubuntu 16.04"* ]]; then
+    echo "this bbr install script used for ubuntu 16.04 only"
+    return 1
+  fi
+
+  if lsmod | grep bbr; then
+    echo "bbr already installed"
+    return 1
+  fi
+}
+
+mu_installs::xenial_bbr::hwe() {
+  mu_installs::xenial_bbr::test || return 0
+  sudo modprobe tcp_bbr
+  sudo apt-get install -y --install-recommends linux-generic-hwe-16.04
+  sudo apt-get autoremove
+}
+
+mu_installs::xenial_bbr::bbr() {
+  mu_installs::xenial_bbr::test || return 0
+
+  echo "net.core.default_qdisc=fq" | sudo tee --append /etc/sysctl.conf
+  echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee --append /etc/sysctl.conf
+
+  sudo sysctl -p
+
+  sysctl net.ipv4.tcp_available_congestion_control
+  sysctl net.ipv4.tcp_congestion_control
+}
